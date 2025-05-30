d1492f141f260c0ed0f404d5ea66a73716594263
Diego Zucchet <diego.zucchet@global66.com>
2025-05-29 20:08:55 -0500

    fix: flag visualization in local

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: '/intelligent-task-manager',
+  base: process.env.NODE_ENV === 'production' ? '/intelligent-task-manager/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
