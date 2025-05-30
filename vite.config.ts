a9684031b3c531723c684a8e833a9f7a5d67bccd
Diego Zucchet <diego.zucchet@global66.com>
2025-05-29 19:00:36 -0500

    fix: resolve base alias at vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
+import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/intelligent-task-manager', // ¡IMPORTANTE! Reemplaza 'gestor-de-tareas' con el nombre exacto de tu repositorio
-  // Si tu repositorio es https://<USERNAME>.github.io/mi-repo/, entonces base es '/mi-repo/'
-  // Si es un proyecto de usuario/organización (https://<USERNAME>.github.io/), entonces base es '/'
+  base: '/intelligent-task-manager',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   build: {
-    outDir: 'docs', // Opcional: GitHub Pages por defecto busca en 'docs/' o 'root/'.
-    // Si prefieres mantener 'dist', tendrás que configurarlo en GitHub Pages manualmente.
-    // Para simplicidad, podemos usar 'docs'.
+    outDir: 'docs',
   },
 })
